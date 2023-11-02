71cc6c268d242334207347c9e30cfb6628d89e43
Colby Jax <colbyjaxcodes@gmail.com>
2023-11-01 18:56:45 -0700

    app now taking in custom local audio files

--- main.py
+++ main.py
@@ -2,21 +2,15 @@ import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Create an array to represent our audio data
-sr = 22050  # Sample rate
-T = 5.0    # Total duration in seconds
-t = np.linspace(0, T, int(T*sr), endpoint=False) # Time array
+# Load an audio file using librosa
+file_path = '../assets/audio/baby_talking.mp3'
+x, sr = librosa.load(file_path, sr=None)  # Load audio file. sr=None ensures original sample rate is used
 
-# Generate a signal with two sine waves and some silence in between
-x = 0.5 * np.sin(2 * np.pi * 220 * t)            # Sine wave at 220 Hz
-x[int(T//2*sr):int((T//2+0.5)*sr)] = 0           # Silence in the middle
-x += 0.5 * np.sin(2 * np.pi * 224 * t)           # Sine wave at 224 Hz
-
-# Let's add some noise
-x += 0.01 * np.random.normal(size=len(t))
+# Create a time array for plotting
+t = np.linspace(0, len(x)/sr, num=len(x))
 
 window_length = 500  # Length of the moving average window
-threshold = 0.4  # Amplitude level below which sound is considered 'quiet'
+threshold = 0.01  # Amplitude level below which sound is considered 'quiet'
 
 # Take the moving average of the absolute values of the signal
 abs_x = np.abs(x)
